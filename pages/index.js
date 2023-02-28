24a4a43c20c7736d77a5b2b9f8424a75da5951cd
bhail300 <bhaile300@gmail.com>
2023-02-27 22:41:21 -0800

    changed code that grabs weather data

--- pages/index.js
+++ pages/index.js
@@ -104,13 +104,13 @@ export default function Home() {
   }
 
   useEffect(() => {
-    if(grabWeather.current === true){
+    //if(grabWeather.current === true){
       fetchWeather();
-    }
+    //}
 
-    return() => {
-      grabWeather.current = true;
-    }
+   // return() => {
+    //  grabWeather.current = true;
+    //}
   }, [])
 
   const current = new Date();
